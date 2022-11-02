3db9c46527492bd40e1ef2fc0bf2d21e524ace5b
ViviAn <applean061516@gmail.com>
2022-11-02 22:50:44 +0800

    Add files via upload

---
+++ build/CMakeFiles/camera_hal3.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/camtest/CameraHAL3Main.cpp" "/root/camtest/build/CMakeFiles/camera_hal3.dir/CameraHAL3Main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "/usr/include/qrb5165"
+  "/usr/src/qrb5165-linux-headers-4.19/usr/include"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/camera_hal3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camera_hal3.dir/CameraHAL3Main.cpp.o"
+  "camera_hal3.pdb"
+  "camera_hal3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camera_hal3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
